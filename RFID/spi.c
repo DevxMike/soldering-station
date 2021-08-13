014e8a6291857677058c78857961e5f087b65d84
mike <michalbaz@poczta.onet.pl>
2021-08-13 18:45:32 +0200

    spi temperature sensor should be ready

--- RFID/spi.c
+++ RFID/spi.c
@@ -1,5 +1,5 @@
 #include "spi.h"
-#define TEMPERATURE_FLAG_BIT 3
+#define TEMPERATURE_FLAG_BIT 2
 
 void set_ss(void){
     PORTB |= (1 << SS);
@@ -8,9 +8,29 @@ void clr_ss(void){
     PORTB &= ~(1 << SS);
 }
 
-uint8_t spi_read_byte(void){
-    return 0;
+void init_spi(void){
+    DDRB |= (1 << SCK) | (1 << MISO);
+    SPCR = (1 << SPE) | (1 << MSTR) | (1 << CPHA) | (1 << SPR0) | (1 << SPIE);
+    set_ss();
 }
+
+uint8_t spi_read_byte(void){
+    SPDR = 0x00;
+    while(!(SPSR & (1 << SPIF)));
+    return SPDR;
+}
+
+uint16_t spi_read_word(void){
+    uint16_t result = 0;
+
+    clr_ss();
+    result |= spi_read_byte() << 8;
+    result |= spi_read_byte();
+    set_ss();
+
+    return result;
+}
+
 uint8_t check_bit(uint16_t word, uint8_t bit){
     if(word &  (1 << bit)){
         return 1;
@@ -19,18 +39,13 @@ uint8_t check_bit(uint16_t word, uint8_t bit){
         return 0;
     }
 }
-uint16_t spi_read_word(void){
-    return 0;
-}
-void init_spi(void){
 
-}
 uint8_t get_temperature(uint16_t* temperature){
     uint16_t spi_read_value = spi_read_word();
 
     if(check_bit(spi_read_value, TEMPERATURE_FLAG_BIT)){
         *temperature = 0;
-        return 0; //no termocouple
+        return 0; //no termocouple attached
     }
     else{
         *temperature = ((spi_read_value & 0x7FF8) >> 3) * 0.25;
