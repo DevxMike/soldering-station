445cdb21203ea26f58309e33e5750114a637b845
mike <michalbaz@poczta.onet.pl>
2021-08-13 16:55:13 +0200

    get_temperature done

--- RFID/spi.c
+++ RFID/spi.c
@@ -1,4 +1,5 @@
 #include "spi.h"
+#define TEMPERATURE_FLAG 0x0004
 
 uint8_t spi_read_byte(void){
     return 0;
@@ -8,4 +9,19 @@ uint8_t check_bit(uint16_t word, uint16_t mask){
 }
 uint16_t spi_read_word(void){
     return 0;
+}
+void init_spi(void){
+
+}
+uint8_t get_temperature(uint16_t* temperature){
+    uint16_t spi_read_value = spi_read_word();
+
+    if(check_bit(spi_read_value, TEMPERATURE_FLAG)){
+        *temperature = 0;
+        return 0; //no termocouple
+    }
+    else{
+        *temperature = spi_read_value;
+        return 1;
+    }
 }
