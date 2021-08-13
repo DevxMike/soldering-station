faf24671c60a92f773a301fc1d65e32b876e513b
mike <michalbaz@poczta.onet.pl>
2021-08-13 16:48:03 +0200

    prototypes added in header and in src

--- RFID/spi.c
+++ RFID/spi.c
@@ -1,1 +1,11 @@
 #include "spi.h"
+
+uint8_t spi_read_byte(void){
+    return 0;
+}
+uint8_t check_bit(uint16_t word, uint16_t mask){
+    return 0;
+}
+uint16_t spi_read_word(void){
+    return 0;
+}
--- RFID/spi.h
+++ RFID/spi.h
@@ -1,4 +1,13 @@
 #ifndef spi_h_
 #define spi_h_
 
+#include <avr/io.h>
+
+#define SS 2
+#define MISO 4
+#define SCK 5
+
+void init_spi(void);
+uint8_t get_temperature(uint16_t* temperature);
+
 #endif
