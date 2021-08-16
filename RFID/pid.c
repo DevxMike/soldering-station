33052618831aa086afc3ca007a5faf8847e4c1ac
mike <michalbaz@poczta.onet.pl>
2021-08-16 14:35:49 +0200

    pid implemented, ready to apply

--- RFID/pid.c
+++ RFID/pid.c
@@ -9,4 +9,41 @@ void init_PID(PID_t* pid, double Kp, double Ti, double Td, double Ts){
     pid->Kd = Kp * Td;
     pid->fraction_1 = (1.0 / (Td * 0.1 + Ts));
     pid->fraction_2 = (0.1 * Td)/(0.1 * Td + Ts);
+    pid->derivative_before = 0.0;
+    pid->sum = pid->error = pid->error_before = 0;
+}
+void compute_error(PID_t* pid, uint16_t error){
+    pid->error_before = pid->error;
+    pid->error = error;
+}
+uint8_t get_PID_pwm(PID_t* pid, uint16_t desired_value, uint16_t actual_value){
+    double P, I, D, temp;
+    int16_t sum;
+
+    compute_error(pid, desired_value - actual_value);
+    P = pid->Kp * pid->error;   
+    
+    if((sum = pid->error + pid->error_before + (int16_t)pid->sum) > 255){
+        pid->sum = 255;
+    }
+    else if((sum < 0)){
+        pid->sum = 0;
+    }
+    else{
+        pid->sum = (uint8_t)sum;
+    }
+
+    I = pid->Ki * pid->sum;
+    D = pid->Kd * (pid->fraction_1 * (pid->error - pid->error_before) + pid->fraction_2*pid->derivative_before);
+    pid->derivative_before = D;
+
+    if((temp = P + I + D) > 255.0){
+        return 255;
+    }
+    else if(temp < 0){
+        return 0;
+    }
+    else{
+        return (uint8_t)temp;
+    }
 }
--- RFID/pid.h
+++ RFID/pid.h
@@ -8,9 +8,11 @@
 
 typedef struct{
     double Kp, Ki, Kd, fraction_1, fraction_2;
+    uint16_t error, error_before, sum;
+    double derivative_before;
 }PID_t;
 
 void init_pwm(void);
 void init_PID(PID_t* pid, double Kp, double Ti, double Td, double Ts);
-
+uint8_t get_PID_pwm(PID_t* pid, uint16_t desired_value, uint16_t actual_value);
 #endif
